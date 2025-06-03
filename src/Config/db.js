e92069ce000419cffb1180e1ef6a3bc090efd73b
Kartik Dwivedi <codewithme.kartik@gmail.com>
2025-06-04 02:04:07 +0530

    updated redis configs and logs

--- src/Config/db.js
+++ src/Config/db.js
@@ -6,7 +6,7 @@ const MONGO_URI = process.env.MONGO_URI;
 const connectToDB = async () => {
   await connect(MONGO_URI)
     .then((conn) => {
-      console.log(`✅ Connected to ${conn.connection.host}`);
+      console.log(`✅ Connected to MongoDB at ${conn.connection.host}`);
     })
     .catch((e) => {
         console.error("❌ Database connection failed ");
--- src/Config/rateLimiter.js
+++ src/Config/rateLimiter.js
@@ -4,14 +4,23 @@ import Redis from "ioredis";
 
 const redisClient = new Redis();
 
+redisClient.on("connect", () => {
+  console.log("✅ Connected to Redis at 127.0.0.1:6379");
+});
+
+redisClient.on("error", (err) => {
+  console.error("❌ Redis connection error:", err.message);
+});
+
 const rateLimiter = (app) => {
   const limiter = rateLimit({
     store: new RedisStore({
       sendCommand: (...args) => redisClient.call(...args),
     }),
-    windowMs: 60 * 1000, // 1 minute
+    windowMs: 60 * 1000, // 1 min
     max: 30, // Limit each IP to 30 requests per minute
-    message: "Too many requests from this IP, please try again after a few minutes.",
+    message:
+      "Too many requests from this IP, please try again after a few minutes.",
   });
 
   app.use(limiter);
